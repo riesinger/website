f7d2834034bf84c07ff05b977975bf3575444a53
riesinger <mail@pascal-riesinger.de>
2021-10-30 00:53:41 +0200

    Include plausible analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import { DefaultSeo } from "components/Seo";
+import Head from "next/head";
 import "../styles/globals.css";
 import "../styles/typography.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <script
+          async
+          defer
+          data-domain="riesinger.dev"
+          src="https://plausible.riesinger.dev/count.js"
+        ></script>
+      </Head>
       <DefaultSeo />
       <Component {...pageProps} />
     </>
