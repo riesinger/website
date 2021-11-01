d1b046a9fffec479e3f4754b27f717e657886131
riesinger <mail@pascal-riesinger.de>
2021-11-01 19:41:28 +0100

    Add bundle analyzer
    An analysis can be created using `ANALYZE=true npm run build`

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-module.exports = {
+const config = {
   reactStrictMode: true,
   trailingSlash: true,
 };
+
+if (process.env.ANALYZE === "true") {
+  const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: true,
+  });
+  console.log("Enabled bundle analysis");
+  module.exports = withBundleAnalyzer(config);
+} else {
+  module.exports = config;
+}
