e88444e6f811a2ebd0517e1ac929b80351519175
Pascal Riesinger <mail@pascal-riesinger.de>
2024-08-20 20:06:04 +0200

    chore: remove framer-motion

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -3,7 +3,6 @@ import PostLinkList from "@/components/PostLinkList";
 import { getAllFilesFrontMatter } from "@/lib/mdx";
 import { Post, PostType } from "@/models/Post";
 import { cva } from "class-variance-authority";
-import { motion } from "framer-motion";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Link from "next/link";
 import Layout from "../components/layout/Layout";
@@ -34,12 +33,7 @@ const Posts = ({
           </h2>
           <ul className="list-none ml-0 space-y-6">
             {featured.map((post, i) => (
-              <motion.li
-                key={post.slug}
-                initial={{ y: 40, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.4, delay: (i + 1) * 0.1 }}
-              >
+              <li key={post.title}>
                 <Link href={`/posts/${post.slug}`}>
                   <div className="bg-gray-50 dark:bg-gray-900 rounded-2xl shadow-sm px-6 py-8 border-2 dark:border-gray-800">
                     <h2
@@ -52,7 +46,7 @@ const Posts = ({
                     <p className="leading-normal">{post.subtitle}</p>
                   </div>
                 </Link>
-              </motion.li>
+              </li>
             ))}
           </ul>
         </section>
